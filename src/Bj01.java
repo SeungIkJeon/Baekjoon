82826700a0fd719b52d085f3ce9348c6112a280f
SeungIkJeon <38490609+SeungIkJeon@users.noreply.github.com>
2019-09-16 14:38:09 +0900

    commit

---
+++ src/Bj01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bj01 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int count = input.nextInt();
+		int a1 = 0;
+		int a2 = 0;
+		
+		for(int i = 1; i<=count; i++) {
+			a1 = input.nextInt();
+			a2 = input.nextInt();
+			System.out.println("Case #"+i+": "+ a1+" +"+a2+" ="+(a1+a2);
+			i++;
+		}
+
+	}
+
+}
