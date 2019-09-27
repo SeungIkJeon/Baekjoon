1a9afc7275936a67c71c21e2cf009e5cdc70b756
SeungIkJeon <38490609+SeungIkJeon@users.noreply.github.com>
2019-09-27 13:58:07 +0900

    중간과정1

---
+++ src/Bj02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bj02 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int count = input.nextInt();
+		int arr[] = new int[count];
+		for(int i=0; i<count; i++) {
+			arr[i] = input.nextInt();
+		}
+		for(int i=0; i<count; i++) {
+			
+		}
+	}
+
+}
