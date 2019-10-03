60e26abfb1eaa3c4a929539f301fc7691c4d4d4a
SeungIkJeon <38490609+SeungIkJeon@users.noreply.github.com>
2019-10-03 17:14:09 +0900

    풀이(아스키코드)

---
+++ src/Bj03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Bj03 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String asc = input.next();
+		int result = asc.charAt(0);
+		
+		System.out.println(result);
+	}
+
+}
